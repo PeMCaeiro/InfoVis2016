9bf97b971ba2456bcfa50a20bee152dff02bbaa0
P.M.C <pedro.miguel14@gmail.com>
2016-11-28 17:36:23 +0000

    Initial commit - html, css and preprocessed data

---
+++ js/attribute-button.js
@@ -0,0 +1,18 @@
+// Prototype Attribute Button
+function Attr_Button(attribute, gender){
+
+   // Add object properties like this
+   this.attribute = attribute;
+   //this.gender = gender;
+}
+
+// Add methods like this.  All objects of this type will be able to invoke this
+Person.prototype.speak = function(){
+    alert("Howdy, my name is" + this.name);
+};
+
+// Instantiate new objects with 'new'
+var person = new Person("Bob", "M");
+
+// Invoke methods like this
+person.speak(); // alerts "Howdy, my name is Bob"
