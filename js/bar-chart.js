44cbe2ec2571e3769f51bf047e9ce12cf85ebf14
PeMCaeiro <pedro.miguel14@gmail.com>
2016-11-29 17:08:26 +0000

    Data now being pre-processed correctly. Simple Bar Graph. TODO: Change to Grouped Bars.

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,98 +1,87 @@
-// Prototype Attribute Button
-function BarChart(){
+//  Prototype BarChart
+
+function BarChart(attribute){
 
    // Add object properties like this
-   //this.attribute = attribute;
+   this.attribute = attribute;
+   this.countries = new Array();
 }
 
+// Attribute Setter
+BarChart.prototype.setAttribute = function(new_attr){
+    this.attribute = new_attr;
+};
+
+// Attribute Getter
+BarChart.prototype.getAttribute = function(){
+    return this.attribute;
+};
+
+
 // Add methods like this.  All objects of this type will be able to invoke this
-Person.prototype.draw = function(data){
-	
-	console.log(data);
+BarChart.prototype.draw = function(data){
 
 	//Initial w and h
-    var w = 450;
+    var w = 800;
     var h = 300;
 
     //Append the svg object to the specified div in the body of the page
-    var svg = d3.select("#rank_chart")
+    var svg = d3.select("#bar_chart")
         .append("svg")
         .attr("width",w)
         .attr("height",h);
-          
-    //var padding = 30;
-    //var bar_w = 15;
 
     //Create initial variables from svg
     svg = d3.select("svg"),
-    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    margin = {top: 20, right: 80, bottom: 30, left: 110},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom;
 
-            //set the Ranges
-        var x = d3.scaleTime().range([0, width]);
-        var y = d3.scaleLinear().range([height, 0]);
+    //Get attribute to use for bars from object
+    var attr = this.attribute;
 
-    //  AXES SCALE AND VARS
+    //set the Ranges
+    var x = d3.scaleBand().range([0, width]).padding(0.1);
+    var y = d3.scaleLinear().range([height, 0]);
 
-    //Ordinal Attribute
-    var xscale = d3.scaleBand()
-        .domain(istidArray)
-        .range([padding,w-padding])
-        .padding(0.1);
+    // selects the svg object of the specified div in the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    svg = d3.select("#bar_chart").select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-    //Quantitative Attribute
-    var yscale = d3.scaleLinear()
-        .domain([getMaxOfArray(upointsArray),0])
-        .range([padding, h-padding]);
 
-    var xaxis = d3.axisBottom()
-        .scale(xscale);
+    // Scale the range of the data
+    //console.log("MIN, MAX: " + d3.extent(data, function(d) { return d[attr]; }) );
 
-    var yaxis = d3.axisLeft()
-        .scale(yscale);
+    x.domain( data.map(function(d) { return d.year; }) );
+    y.domain( [0, d3.max(data, function(d) { return d[attr]; }) ] );
 
-    // Setup the tool tip.  Note that this is just one example, and that many styling options are available.
-    // See original documentation for more details on styling: http://labratrevenge.com/d3-tip/
-    var tool_tip = d3.tip()
-        .attr("class", "d3-tip")
-        .offset([-8, 0])
-        .html(function(d) { return "Player: " + d.istid + "</br>" + "</br>" +  "Points: " + d.upoints ; });
-        //.html(function(d) { return "Points: " + d.upoints ; });
-            
-    d3.select("svg").call(tool_tip);
+    // Axis vars
+    var xAxis = d3.axisBottom(x);
+    var yAxis = d3.axisLeft(y);
 
     //  DRAW BARS
           
     var bar = svg.selectAll("g")
         .data(data)
         .enter()
-        .append("g")
-        .attr("transform", function(d) { return "translate(" + xscale(d.istid.toString()) + ", 0)"; });
+        .append("rect")
+        .attr("x", function(d) { return x(d.year); } )
+        .attr("y", function(d) { return y(d.gdp); } )
+        .attr("height", function(d) { return height - y(d.gdp); })
+        .attr("width", x.bandwidth());
 
-    bar.append("rect")
-        .attr("y", function(d) { return yscale(d.upoints); } )
-        .attr("height", function(d) { return h - padding - yscale(d.upoints); })
-        .attr("width", xscale.bandwidth())
-        .on('mouseover', tool_tip.show)
-        .on('mouseout', tool_tip.hide);
-
-    // DRAW AXES
-
+    // Add the X Axis
     svg.append("g")
-        .attr("transform","translate(30,0)")  
-        .attr("class","y axis")
-        .call(yaxis);
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
+    // Add the Y Axis
     svg.append("g")
-        .attr("transform","translate(0," + (h - padding) + ")")
-        .attr("class","x axis")
-        .call(xaxis);
-        //.selectAll("text")
-        //.attr("y", 0)
-        //.attr("x", 9)
-        //.attr("dy", ".35em")
-        //.attr("transform", "rotate(40)")
-        //.style("text-anchor", "start");
+        .call(yAxis);
 
 };
--- js/main.js
+++ js/main.js
@@ -1,75 +1,18 @@
-//	Global Variables
+$(window).load(function(){
 
-var euData = new Array();
-var originsData = new Array();
-var mainData = new Array();
+//	Draw Initial State - only runs after data is loaded
 
-//	1. Retrieve Data
-
-d3.json("data/originsDestinations.json", function(data) {
-	//console.log(data);  
-	//  Get input data and load it into memory
-    for(var i=0;i<data.length;i++){
-    	originsData[i] = new Object();
-
-        originsData[i]["country"] = data[i]["country"];
-        originsData[i]["destination"] = data[i]["destination"];
-        originsData[i]["numTrips"] = data[i]["numberTrips"];
-        originsData[i]["year"] = data[i]["year"];
-
-    }
-    //console.log(originsData);
-});
-
-d3.json("data/mainTable.json", function(data) {
-	//console.log(data);  
-	//  Get input data and load it into memory
-    for(var i=0;i<data.length;i++){
-    	mainData[i] = new Object();
-
-        mainData[i]["arrivals"] = data[i]["arrivalsWW"];
-        mainData[i]["country"] = data[i]["country"];
-        mainData[i]["departures"] = data[i]["departuresWW"];
-        mainData[i]["directToGDP"] = data[i]["directContributionToGdpDollars"];
-        mainData[i]["numJobs"] = data[i]["employmentNumberJobs"];
-        mainData[i]["expenditures"] = data[i]["expendituresDollars"];
-        mainData[i]["gdp"] = data[i]["gdp"];
-        mainData[i]["netOccupancyRate"] = data[i]["netOccupancyRate"];
-        mainData[i]["percentToGdp"] = data[i]["percentageContributionGdp"];
-        mainData[i]["population"] = data[i]["population"];
-        mainData[i]["year"] = data[i]["year"];
-        mainData[i]["receipts"] = data[i]["receiptsDollars"];
-
-    }
-    //console.log(mainData);
-});
-
-d3.json("data/eu.json", function(data) {
-	//console.log(data);  
-	//  Get input data and load it into memory
-    for(var i=0;i<data.length;i++){
-    	euData[i] = new Object();
-
-        euData[i]["arrivals"] = data[i]["arrivalsWW"];
-        euData[i]["country"] = data[i]["country"];
-        euData[i]["departures"] = data[i]["departuresWW"];
-        euData[i]["directToGDP"] = data[i]["directContributionToGdpDollars"];
-        euData[i]["numJobs"] = data[i]["employmentNumberJobs"];
-        euData[i]["expenditures"] = data[i]["expendituresDollars"];
-        euData[i]["gdp"] = data[i]["gdp"];
-        euData[i]["percentToGdp"] = data[i]["percentageContributionGdp"];
-        euData[i]["population"] = data[i]["population"];
-        euData[i]["year"] = data[i]["year"];
-        euData[i]["receipts"] = data[i]["receiptsDollars"];
-
-    }
-    //console.log(euData);
-});
-
-//	2. Draw Initial State
+console.log(euData);
 
 // Instantiate new objects with 'new'
-//var bar_chart = new Bar_Chart("Bob", "M");
+
+var bar_chart = new BarChart("gdp");
 
 // Invoke methods like this
-//person.draw();
+
+//bar_chart.setAttribute("numJobs");
+console.log(bar_chart.getAttribute());
+
+bar_chart.draw(euData);
+
+});
