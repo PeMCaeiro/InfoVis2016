6279fae2ccfb9fcd40c6899474206d69a4e9b28c
txexas <andresantos@Andres-MacBook-Pro.local>
2016-12-21 23:55:28 +0000

    need help

--- js/choropleth-map.js
+++ js/choropleth-map.js
@@ -15,10 +15,9 @@ ChoroplethMap.prototype.getAttribute = function(){
     return this.attribute;
 };
 
-//ChoroplethMap.prototype.draw = function(data){
+ChoroplethMap.prototype.draw = function(data){
 	
-	/*var w = 900;
-    var h = 500;
+	/*
 	
 	var projection = d3.geoMercator()
     .scale(1070)
@@ -26,30 +25,87 @@ ChoroplethMap.prototype.getAttribute = function(){
 
 	var path = d3.geo.path()
     .projection(projection);
-	
-	var svg = d3.select("#choropleth_map")
+//}*/
+
+var w = 960;
+var h = 600;
+
+var svg = d3.select("#choropleth_map")
         .append("svg")
         .attr("width",w)
-        .attr("height",h);*/
+        .attr("height",h);
 		
-	var europePopMap = SimpleMapD3({
-    container: '.simple-map-d3-europe-pop-map',
-    datasource: 'example-data/europe-population-density-geocommons.geo.json',
-    colorSet: 'Spectral',
-    colorOn: true,
-    colorProperty: 'population',
-    colorReverse: true,
-    projection: 'azimuthalEqualArea',
-    rotation: [0, 0, -20],
-    canvasDragOn: true,
-    tooltipContent: function(d) {
-      var p = d.properties;
-      return '<h5>' + p.country + '</h5>' +
-        p.population + ' population per square kilometer';
-    }
-  });
-  
-//}
-		
+svg = d3.select("#svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var unemployment = d3.map();
+
+var path = d3.geoPath();
+
+var x = d3.scaleLinear()
+    .domain([1, 10])
+    .rangeRound([600, 860]);
+
+var color = d3.scaleThreshold()
+    .domain(d3.range(2, 10))
+    .range(d3.schemeBlues[9]);
+
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(0,40)");
+
+g.selectAll("rect")
+  .data(color.range().map(function(d) {
+      d = color.invertExtent(d);
+      if (d[0] == null) d[0] = x.domain()[0];
+      if (d[1] == null) d[1] = x.domain()[1];
+      return d;
+    }))
+  .enter().append("rect")
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function(d) { return color(d[0]); });
+
+g.append("text")
+    .attr("class", "caption")
+    .attr("x", x.range()[0])
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Unemployment rate");
+
+g.call(d3.axisBottom(x)
+    .tickSize(13)
+    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickValues(color.domain()))
+  .select(".domain")
+    .remove();
+
+d3.queue()
+    .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+    .defer(d3.tsv, "TODO", function(d) { unemployment.set(d.id, +d.rate); })
+    .await(ready);
+
+function ready(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+      .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
+      .attr("d", path)
+    .append("title")
+      .text(function(d) { return d.rate + "%"; });
+
+  svg.append("path")
+      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+      .attr("class", "states")
+      .attr("d", path);
+}   
 
 	
